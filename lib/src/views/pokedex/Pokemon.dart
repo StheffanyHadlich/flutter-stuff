1d6a6748c6d4db768eeeef88e00170cb16e1b1ee
Stheffany Hadlich <stheffany@codeminer42.com>
2020-04-29 16:12:05 -0300

    feat: Display pokemons pictures

--- lib/src/views/pokedex/Pokemon.dart
+++ lib/src/views/pokedex/Pokemon.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:i18next/i18next.dart';
 import '../../models/PokemonsModel.dart';
+import 'package:http/http.dart' as http;
+
 
 class Pokemon extends StatelessWidget {
   final PokemonsModel pokemon;
@@ -14,10 +18,7 @@ class Pokemon extends StatelessWidget {
       child: Center(
         child: Column(
           children: <Widget>[
-            Image(
-              image: NetworkImage(
-                  'https://www.comoaprenderdesenhar.com.br/wp-content/uploads/2015/09/Como-Desenhar-o-Pikachu-de-Pok%C3%A9mon-1.jpg'),
-            ),
+            PokemonPhoto(pokemon.url),
             Text(I18Next.of(context).t('pokedex:name')),
             Text(
               pokemon.name,
@@ -30,5 +31,46 @@ class Pokemon extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class PokemonPhoto extends StatelessWidget {
+  final String url;
+
+  PokemonPhoto(this.url);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getPokemonPhoto(url),
+      builder: (_, snapshot) {
+        if (snapshot.hasData) {
+          final photo = snapshot.data;
+          print(photo);
+          return Image(
+            image: NetworkImage(photo),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
+
+Future<String> getPokemonPhoto(url) async {
+
+  try {
+    final response = await http.get(url);
+    final data = jsonDecode(response.body);
+    final pokemons = data['sprites'];
+    return pokemons['front_default'];
+    
+  } catch (e) {
+    throw Exception(e);
   }
 }
